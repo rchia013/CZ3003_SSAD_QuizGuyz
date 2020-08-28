38d2bb679e9b057351ec8582b5e9f010fa57905c
chavalitpramotedham <chavalit@pramotedham.com>
2020-08-28 14:18:23 +0800

    28 Aug 2.18pm

---
+++ Assets/ActivatedBlock.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivatedBlock : MonoBehaviour
+{
+    public Material material3;
+    public Material material2;
+    public Material material1;
+
+    MeshRenderer rend;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = gameObject.GetComponent<MeshRenderer>();
+        rb = gameObject.GetComponent<Rigidbody>();
+
+        rb.isKinematic = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            transform.Translate(Vector3.down * 0.1f);
+            print("Give Question");
+
+            StartCoroutine("Countdown");
+        }
+    }
+
+    IEnumerator Countdown()
+    { 
+
+        int counter = 5;
+
+        while (counter > 0)
+        {
+            yield return new WaitForSeconds(1);
+
+            counter--;
+
+            Material[] materials = rend.materials;
+
+            switch (counter)
+            {
+                case 3:
+                    materials[0] = material3;
+                    break;
+
+                case 2:
+                    materials[0] = material2;
+                    break;
+
+                case 1:
+                    materials[0] = material1;
+                    break;
+
+                case 0:
+                    rb.isKinematic = false;
+                    break;
+            }
+
+            rend.materials = materials;
+        }
+    }
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{ 
+
+    public float speed = 5f;
+
+    public float turnSmoothTime = 0.1f;
+
+    float turnSmoothVelocity;
+   
+    // Update is called once per frame
+    void Update ()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical);
+
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+
+            transform.Translate(direction * Time.deltaTime * speed);
+
+            
+        }
+            
+
+
+        //    //Moves Forward and back along z axis                           //Up/Down
+        //transform.Translate(Vector3.forward * Time.deltaTime * Input.GetAxis("Vertical")* moveSpeed);
+        //    //Moves Left and right along x Axis                               //Left/Right
+        //transform.Translate(Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal")* moveSpeed);      
+    }
+}
